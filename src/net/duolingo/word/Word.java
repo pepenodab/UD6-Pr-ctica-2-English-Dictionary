6fab7cbf141ede9da72473958ec5c50ba000b048
CamiloP4ez <usr3765@salesianos-lacuesta.net>
2024-05-09 23:25:49 +0100

    feat(word): creating word class

--- src/net/duolingo/word/Word.java
+++ src/net/duolingo/word/Word.java
@@ -2,11 +2,12 @@ package net.duolingo.word;
 
 import net.duolingo.dictionary.Dictionary;
 import java.util.Set;
+
+import javax.swing.JOptionPane;
+
 import java.util.HashSet;
-import java.util.Map;
 
 public class Word {
-    // private String palabra;
 
     public static String getInitial(String word) {
 
@@ -18,29 +19,43 @@ public class Word {
         return word.toLowerCase().replaceAll("[^A-Za-z]", "");
     }
 
+    public static Set<String> setCopier(String word) {
+        Set<String> words = new HashSet<>(Dictionary.getDictionary().get(getInitial(word)));
+        return new HashSet<>(words);
+    }
+
     public static void addWord(String word) {
-        if (!Dictionary.getDictionary().containsKey(getInitial(word))) {
-
+        if (Dictionary.getDictionary().containsKey(getInitial(word))) {
+            Set<String> words = setCopier(word);
+            words.add(word);
+            Dictionary.addSet(getInitial(word), words);
+        } else {
             Set<String> words = new HashSet<>();
-            words.add(wordCleaner(word));
-            Dictionary.getDictionary().put(getInitial(word), words);
+            words.add(word);
+            Dictionary.addSet(getInitial(word), words);
+        }
+    }
 
-        } else if (Dictionary.getDictionary().containsKey(getInitial(word))) {
-            Set<String> words = Dictionary.getDictionary().getOrDefault(getInitial(word), new HashSet<>());
-            words.add(wordCleaner(word));
-            Dictionary.getDictionary().put(getInitial(word), words);
-
+    public static boolean isThereAWord(String word) {
+        Set<String> words = setCopier(word);
+        if (words.contains(word)) {
+            JOptionPane.showMessageDialog(null, "la palabra  fue encontrada");
+            return true;
+        } else {
+            JOptionPane.showMessageDialog(null, "la palabra no fue encontrada");
+            return false;
         }
 
     }
 
-    public static void addWordByCamilo(String word) {
-        // Dictionary.setDictionary(getInitial(word), wor);
-        Set<String> words = new HashSet<>(Dictionary.getDictionary().get(getInitial(word)));
-        System.out.println(words);
-
-        if (Dictionary.getDictionary().containsKey(getInitial(word))) {
-
+    public static void deleteWord(String word) {
+        Set<String> words = setCopier(word);
+        if (words.contains(word)) {
+            words.remove(word);
+            Dictionary.addSet(getInitial(word), words);
+            JOptionPane.showMessageDialog(null, "la palabra  fue eliminada");
+        } else {
+            JOptionPane.showMessageDialog(null, "la palabra no fue encontrada");
         }
     }
 
